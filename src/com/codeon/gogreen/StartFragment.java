4df076c38c14dada3ff65465be5e3f6769fc2bad
Oksisane <ehsanmasdar@gmail.com>
2014-06-14 04:47:49 -0500

    Basic implementation for frontpage data

--- src/com/codeon/gogreen/StartFragment.java
+++ src/com/codeon/gogreen/StartFragment.java
@@ -1,10 +1,13 @@
 package com.codeon.gogreen;
 
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.support.v4.app.Fragment;
 
 public class StartFragment extends Fragment {
@@ -14,6 +17,20 @@ public class StartFragment extends Fragment {
 		View rootView = inflater.inflate(R.layout.fragment_home2, container,
 				false);
 		return rootView;
+	}
+	@Override
+	public void onStart(){
+		super.onStart();
+		SharedPreferences s = PreferenceManager.getDefaultSharedPreferences(getActivity());
+		int bikingtotalint = s.getInt("bikingtotal", 0);
+		int parkstotalint = s.getInt("parks", 0);
+		int totalpoints = bikingtotalint + parkstotalint;
+		TextView bikingtotal = (TextView) getView().findViewById(R.id.);
+		TextView parkstotal = (TextView) getView().findViewById(R.id.);
+		TextView total = (TextView) getView().findViewById(R.id.);
+		bikingtotal.setText("Your biking points are: " + bikingtotal);
+		parkstotal.setText("Your parks points are: " + parkstotal);
+		total.setText("Your total points are: " + totalpoints);
 	}
 	
 }
