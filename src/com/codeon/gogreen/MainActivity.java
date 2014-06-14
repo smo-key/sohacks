53e8f862134b5eab1e99a2eae7ae47ff6baca99b
Oksisane <ehsanmasdar@gmail.com>
2014-06-13 21:41:44 -0500

    Added actionbar

--- src/com/codeon/gogreen/MainActivity.java
+++ src/com/codeon/gogreen/MainActivity.java
@@ -14,14 +14,18 @@ import com.google.android.gms.location.DetectedActivity;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,9 +34,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.os.Build;
 
-public class MainActivity extends FragmentActivity implements ConnectionCallbacks, OnConnectionFailedListener{
+public class MainActivity extends ActionBarActivity implements ConnectionCallbacks, OnConnectionFailedListener{
 	private final BroadcastReceiver receiver = new BroadcastReceiver() {
 		@Override
 		public void onReceive(Context context, Intent intent) {
@@ -44,6 +51,10 @@ public class MainActivity extends FragmentActivity implements ConnectionCallback
 	private ActivityRecognitionClient mActivityRecognitionClient;
 	private PendingIntent mActivityRecognitionPendingIntent;
 	private String googlekey = "AIzaSyBV15lTOpTwK2Jkv_zxWwfRyU8DsasucAY";
+	private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private String[] drawerNames = {"Parks"};
+    private ActionBarDrawerToggle mDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,20 +62,52 @@ public class MainActivity extends FragmentActivity implements ConnectionCallback
         IntentFilter filter = new IntentFilter();
 		filter.addAction("com.codeon.gogreen.ActivityRecieved");
 		registerReceiver(receiver, filter);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new HomeFragment())
-                    .commit();
-        }
+     
         //Activity Recog
         mActivityRecognitionClient =
                 new ActivityRecognitionClient(getApplicationContext(), (com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks) this, this);
         Intent intent = new Intent(getApplicationContext(), ActivityRecognitionIntentService.class);
         mActivityRecognitionClient.connect();
         mActivityRecognitionPendingIntent = PendingIntent.getService(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
- 
-    }
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, drawerNames));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+                ) {
 
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -83,6 +126,9 @@ public class MainActivity extends FragmentActivity implements ConnectionCallback
         if (id == R.id.action_settings) {
             return true;
         }
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+          }
         return super.onOptionsItemSelected(item);
     }
     private void changeText (int action){
@@ -104,7 +150,27 @@ public class MainActivity extends FragmentActivity implements ConnectionCallback
 		return "";
 	}
 
+   	/** Swaps fragments in the main content view */
+   	private void selectItem(int position) {
+   	    // Create a new fragment and specify the planet to show based on position
+   		Fragment fragment;
+   	    switch (position){
+   	    	case 0:
+   	    		fragment = new HomeFragment();
+   	    	default:
+   	    		fragment = new HomeFragment();
+   	    }
+   	    // Insert the fragment by replacing any existing fragment
+   	    FragmentManager fragmentManager = getSupportFragmentManager();
+   	    fragmentManager.beginTransaction()
+   	                   .replace(R.id.container, fragment)
+   	                   .commit();
 
+   	    // Highlight the selected item, update the title, and close the drawer
+   	    mDrawerList.setItemChecked(position, true);
+   	    setTitle(drawerNames[position]);
+   	    mDrawerLayout.closeDrawer(mDrawerList);
+   	}
 	@Override
 	public void onConnectionFailed(ConnectionResult arg0) {
 		// TODO Auto-generated method stub
@@ -129,7 +195,17 @@ public class MainActivity extends FragmentActivity implements ConnectionCallback
 		
 	}
 	
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+	    @Override
+	    public void onItemClick(AdapterView parent, View view, int position, long id) {
+	        selectItem(position);
+	    }
 
-
+	}
+	@Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
 }
