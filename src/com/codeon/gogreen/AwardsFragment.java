87b7cb0f9022a6a6d7e8391b69ed525c50b4703c
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-06-14 04:25:51 -0500

    Home Page Updated

--- src/com/codeon/gogreen/AwardsFragment.java
+++ src/com/codeon/gogreen/AwardsFragment.java
@@ -2,17 +2,17 @@ package com.codeon.gogreen;
 
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.app.Fragment;
 
-public class StartFragment extends Fragment {
+public class AwardsFragment extends Fragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_home2, container,
-				false);
+		View rootView = inflater.inflate(R.layout.fragment_bicycle, container,
+				false);	
 		return rootView;
-	}
+	}    
 }
